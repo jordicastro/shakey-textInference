1d07cde416237cb9be909444d871331438ca7019
Jordi Castro <jordi.castro003@gmail.com>
2025-03-26 21:59:48 -0500

    example romeo & juliet quotes on different ngrams

--- ngrams.py
+++ ngrams.py
@@ -4,6 +4,63 @@ import random
 
 import sys
 
+def update_successor_map(window, successor_map, ngram) -> dict:
+    if ngram == 2:
+        if len(window) == 2:
+            if window[0] not in successor_map:
+                successor_map[window[0]] = [window[1]]
+            else:
+                successor_map[window[0]].append(window[1])
+            window.pop(0)
+    else:
+        if len(window) == ngram:
+            key = tuple(window[:-1])
+            value = window[-1]
+            if key not in successor_map:
+                successor_map[key] = [value]
+            else:
+                successor_map[key].append(value)
+            window.pop(0)
+
+
+    return successor_map
+
+def print_examples(successor_map, ngram):
+
+    seed_words1 = ['romeo', 'and', 'juliet', 'by', 'william', 'shakespeare']
+    seed_words2 = ['thus', 'with', 'a', 'kiss', 'i', 'die']
+    seed_words3 = ['o', 'romeo', 'romeo', 'wherefore', 'art', 'thou']
+    seed_words4 = ['a', 'pair', 'of', 'star-crossed', 'lovers', 'take']
+    seed_words5 = ['in', 'fair', 'verona', 'where', 'we', 'lay']
+    words = seed_words2[:ngram-1]
+
+    print("WORDS: ", words)
+
+    if ngram == 2:
+        if words[0] in successor_map:
+            print("SUCCESSOR MAP: ", successor_map[words[0]])
+            print("WEIGHTED CHOICE: ", random.choice(successor_map[words[0]]))
+    elif ngram >= 3:
+        key = tuple(words)
+        if key in successor_map:
+            print("SUCCESSOR MAP: ", successor_map[key])
+            print("WEIGHTED CHOICE: ", random.choice(successor_map[key]))
+        else:
+            print("Key not found")
+
+    print("------------------")
+    for i in range(50):
+        print(words[0], end=' ')
+        if ngram == 2:
+            next_word = random.choice(successor_map[words[0]])
+            words[0] = next_word
+        elif ngram >= 3:
+            key = tuple(words)
+            if key in successor_map:
+                next_word = random.choice(successor_map[key])
+                words.pop(0)
+                words.append(next_word)
+    print("\n----------------")
 
 def main():
 
@@ -28,54 +85,11 @@ def main():
             cleaned_word = word.strip(punctuation).lower()
             window.append(cleaned_word)
 
-            # if len(window) == 2:
-            #     if window[0] not in successor_map:
-            #         successor_map[window[0]] = [window[1]]
-            #     else:
-            #         successor_map[window[0]].append(window[1])
-            #     window.pop(0)
-
-            if len(window) == 3:
-                key = (window[0], window[1])
-                value = window[2]
-                if key not in successor_map:
-                    successor_map[key] = [value]
-                else:
-                    successor_map[key].append(value)
-                window.pop(0)
+            successor_map = update_successor_map(window, successor_map, ngram)
 
 
-    # random.seed(1)
 
-    # print(successor_map['romeo']) 
-    # print(list(successor_map.keys())[:10])  # Print the first 10 keys
-
-    # limit print to first 10
-    if (ngram == 2):
-        print(successor_map['romeo'][:10])
-    elif (ngram == 3):
-        key = ('romeo', 'and')
-        if key in successor_map:
-            print( successor_map[key] )
-            print( random.choice(successor_map[key]) )
-        else:
-            print("Key not found")
-
-    # word0 = input("Enter a word: ")
-    # word1 = random.choice(successor_map[word0])
-
-    # print(word1)
-
-    word1 = 'romeo'
-    word2 = 'and'
-
-    for i in range(50):
-        print(word1, end=' ')
-        successors = successor_map[(word1, word2)]
-        word3 = random.choice(successors)
-
-        word1 = word2
-        word2 = word3
+    print_examples(successor_map, ngram)
 
 if __name__ == "__main__":
     main()
